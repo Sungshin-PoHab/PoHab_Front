42cbc6cf87c66485c978b11ef6ab639f570a0f21
zinzo1019 <topjoy22@naver.com>
2022-08-09 22:14:23 +0900

    feat: 로그인 처리를 위한 OAuth.js & Auth.js 파일 추가

---
+++ src/pages/Auth.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Auth(SpecificComponent, option) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userEmail = window.localStorage.getItem("userEmail");
+
+    if (option) {
+      if (!userEmail) {
+        navigate("/login");
+      }
+    }
+  });
+  return <SpecificComponent />;
+}
+
+export default Auth;

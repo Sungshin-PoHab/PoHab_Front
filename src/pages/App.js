db8ef48985508f3d5f11a4c06229acb957c46cc8
zinzo1019 <topjoy22@naver.com>
2022-08-09 22:14:40 +0900

    기타 설정 파일

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Hello from "../components/Hello";
+import Home from "./Home";
+import Login from "./Login";
+import Oauth from "../components/OAuth";
+import Auth from "./Auth";
+
 function App() {
   return (
-    <div>
-      <Hello />
-    </div>
+    // <div>
+    //   <Hello />
+    // </div>
+    <Routes>
+      <Route path="/" element={Auth(Home, true)} />
+      <Route path="/hello" element={Auth(Hello, true)} />
+      <Route path="/login" element={Auth(Login, null)} />
+      <Route path="/oauth" element={Auth(Oauth, null)} />
+    </Routes>
   );
 }
 

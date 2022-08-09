f2db0c05b465f2ad4c2be758dd0bd0934e6790f0
eunjungL <dldmswjd322@gmail.com>
2022-08-10 02:56:45 +0900

    feat: Step create feat add

--- src/components/recruit/RecruitStep.js
+++ src/components/recruit/RecruitStep.js
@@ -1,51 +1,82 @@
 import {useState} from "react";
+import axios from "axios";
 
 function RecruitStep() {
     const [stepBtn, setBtnStep] = useState([]);
-    const [stepName, setStepName] = useState("1단계");
-    const [startDate, setStartDate] = useState(new Date(Date.now()).toISOString().split('.')[0]);
-    const [endDate, setEndDate] = useState(new Date(Date.now()).toISOString().split('.')[0])
+    const [btnCount, setBtnCount] = useState(1);
+    const [nowStep, setNowStep] = useState(0);
+    const [startDate, setStartDate] = useState([new Date(Date.now()).toISOString().split('.')[0]]);
+    const [endDate, setEndDate] = useState([new Date(Date.now()).toISOString().split('.')[0]]);
 
-    const handlePlusBtn = () => {
-        setBtnStep(stepBtn.concat(<button>{stepName}</button>));
+    const handleStepBtnClick = (event) => {
+        event.preventDefault();
+
+        setNowStep(event.target.innerHTML - 1);
     }
 
-    const handleStepNameChange = (event) => {
-        const value = event.target.value;
-        setStepName(value);
+    const handlePlusBtn = (event) => {
+        event.preventDefault();
+
+        setBtnStep(stepBtn.concat(<button onClick={handleStepBtnClick}>{btnCount}</button>));
+        setBtnCount(btnCount + 1);
     }
 
     const handleDateChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
 
-        if (name === 'start_date') setStartDate(value);
-        else setEndDate(value);
+        if (name === 'start_date') {
+            startDate[nowStep] = value;
+            setStartDate(startDate);
+        }
+        else {
+            endDate[nowStep] = value;
+            setEndDate(endDate);
+        }
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        alert("submit")
+
+        let body = []
+        for (let i = 0; i < stepBtn.length; i++) {
+            body.push({
+                "step": i + 1,
+                "startDate": startDate[i],
+                "endDate": endDate[i]
+            })
+        }
+
+        console.log(body);
+
+        // 추후 변경 필수
+        let group_id = 'sungshin22'
+        const res = await axios.post(`${group_id}`, body);
+        console.log(res.data);
     }
 
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <div>
                 <h3>모집 일정 등록</h3>
                 <p>모집 단계와 모집 시작 및 모집 마감 일정을 등록할 수 있습니다.</p>
                 <p>모집 단계 이름은 수정이 불가하니 주의해주세요.</p>
             </div>
             <div>
-                <p>모집 단계</p>
-                <input type={"text"} name={"step_name"} onChange={handleStepNameChange} value={stepName}/>
                 {stepBtn}
                 <button onClick={handlePlusBtn}>+</button>
             </div>
             <div>
                 <p>모집 시작 일정</p>
-                <input type={"datetime-local"} name={"start_date"} onChange={handleDateChange} value={startDate}/>
+                <input type={"datetime-local"} name={"start_date"} onChange={handleDateChange} value={startDate[nowStep]}/>
             </div>
             <div>
                 <p>모집 종료 일정</p>
-                <input type={"datetime-local"} name={"end_date"} onChange={handleDateChange} value={endDate}/>
+                <input type={"datetime-local"} name={"end_date"} onChange={handleDateChange} value={endDate[nowStep]}/>
             </div>
             <input type={"submit"} name={"step_submit"} value={"모집 부서 이어서 등록 >"}/>
-        </div>
+        </form>
     )
 }
 

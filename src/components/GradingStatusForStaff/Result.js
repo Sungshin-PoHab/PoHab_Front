b2667cefab4e6d65fca012fbb4bddc09c612f59d
zinzo1019 <topjoy22@naver.com>
2022-08-13 20:37:11 +0900

    feat: 평균 / 최고 점수 / 최저 점수 추가

---
+++ src/components/GradingStatusForStaff/Result.js
@@ -0,0 +1,16 @@
+import './Result.css';
+
+function Result(props) {
+
+  return (
+    <div className='result-div'>
+      <h5 className='result-h5' style={{marginLeft: '8%', marginRight: '25%'}}>전체 평균: { props.overallAvg }</h5>
+      <h5 className='result-h5' style={{marginRight: '25%'}}>최고 점수: { props.highScore }</h5>
+      <h5 className='result-h5'>최저 점수: { props.lowestScore }</h5>
+
+    </div>
+
+  );
+}
+
+export default Result;

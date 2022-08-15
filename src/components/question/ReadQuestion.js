7193219ea8b482a924ea2aa806e0c1fc045c1d47
eunjungL <dldmswjd322@gmail.com>
2022-08-16 01:55:42 +0900

    refactoring: Read Question code rafactoring

--- src/components/question/ReadQuestion.js
+++ src/components/question/ReadQuestion.js
@@ -8,39 +8,35 @@ function ReadQuestion() {
 
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [personalQuestions, setPersonalQuestions] = useState('');
+  const [personalQuestions, setPersonalQuestions] = useState([]);
 
   const { party_id } = useParams();
-  const description_id = useRef(0);
-  const personal_id = useRef(0);
 
   const deleteQuestion = async (question_id) => {
     if (window.confirm('해당 질문을 정말 삭제하시겠습니까?')) {
       await instance.delete('/question/' + question_id);
+      setPersonalQuestions(
+        personalQuestions.filter((question) => question.id !== question_id)
+      );
+    } else {
+      alert('취소 되었습니다.');
     }
   };
 
-  const getDepartments = async () => {
-    const res = await instance.get('/recruit/department/' + party_id);
+  const fetchDepartmentsAndQuestion = async () => {
+    const res = await instance.get(`/recruit/department/${party_id}`);
 
-    // 설명 id get
-    const description_data = res.data.find((data) => {
-      return data.department === '설명';
-    });
-    description_id.current = description_data.id;
-    // 개인정보 id get
-    const personal_data = res.data.find((data) => {
-      return data.department === '개인정보';
-    });
-    personal_id.current = personal_data.id;
-  };
+    let data = res.data;
+    const personal_id = data.filter((data) => data.department === '개인정보')[0]
+      .id;
+    const description_id = data.filter((data) => data.department === '설명')[0]
+      .id;
 
-  const getQuestion = useCallback(async () => {
-    // 설명 get
-    const description_questions = await instance.get(
-      '/question?department=' + description_id.current
+    const description_question = await instance.get(
+      '/question?department=' + description_id
     );
-    description_questions.data.forEach((data) => {
+    console.log(description_question.data);
+    description_question.data.forEach((data) => {
       if (data.question.startsWith('title '))
         setTitle(data.question.split(' ')[1]);
       else setDescription(data.question.split(' ')[1]);
@@ -48,29 +44,30 @@ function ReadQuestion() {
 
     // 개인정보 질문 get
     const personal_questions = await instance.get(
-      '/question?department=' + personal_id.current
+      '/question?department=' + personal_id
     );
-    setPersonalQuestions(
-      personal_questions.data.map((data) => {
-        return [
-          <div className={'L-row'}>
-            <p>{data.question}</p>
-            <button
-              className={'L-delete-btn'}
-              onClick={() => deleteQuestion(data.id)}
-            >
-              삭제
-            </button>
-          </div>,
-        ];
-      })
-    );
-  }, [deleteQuestion]);
+    setPersonalQuestions(personal_questions.data);
+  };
+
+  const renderPersonal = (personalQuestions) => {
+    return personalQuestions.map((data) => {
+      return [
+        <div className={'L-row'}>
+          <p>{data.question}</p>
+          <button
+            className={'L-delete-btn'}
+            onClick={() => deleteQuestion(data.id)}
+          >
+            삭제
+          </button>
+        </div>,
+      ];
+    });
+  };
 
   useEffect(() => {
-    getDepartments();
-    getQuestion();
-  }, [getQuestion]);
+    fetchDepartmentsAndQuestion();
+  }, []);
 
   return (
     <div className={'L-container'}>
@@ -91,7 +88,7 @@ function ReadQuestion() {
       <div className={'L-description'}>
         <h3 className={'L-description-title'}>개인정보 질문란</h3>
       </div>
-      {personalQuestions}
+      {renderPersonal(personalQuestions)}
       <button
         className={'L-button'}
         onClick={() => navigate(`/question/department/${party_id}`)}

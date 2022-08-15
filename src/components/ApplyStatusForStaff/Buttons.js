531a2e02f38f3994d46f56a854ed1cc6dceb4d76
zinzo1019 <topjoy22@naver.com>
2022-08-16 02:16:51 +0900

    fix: 지원 현황 페이지 필요없는 버튼 제거

--- src/components/ApplyStatusForStaff/Buttons.js
+++ src/components/ApplyStatusForStaff/Buttons.js
@@ -1,16 +1,24 @@
+import { useParams } from 'react-router-dom';
+
 import '../../assets/ApplyStatusForStaff/Buttons.css';
 
 function Buttons(props) {
 
+  let params = useParams();
+
+  const onClick = () => {
+    console.log('button clicked');
+    window.location.href=`http://localhost:3000/grading/announcePNP/${params.department}/${params.step}`;
+  }
+
   return (
-    <div className='button-div'>
+    <div className='z-button-div'>
       <h4 style={{ marginBottom: '4%' }}>지원 관리</h4>
-      <div className='management_btn'>
-        <button className='buttons' style={{ float: 'left' }}>지원서 마감하기</button>
-        <button className='buttons' style={{ float: 'right' }}>지원서 평가 기준 입력하기</button>
-        <button className='buttons' style={{ float: 'left' }}>서류 합격자 발표하러 가기</button>
-        <button className='buttons' style={{ float: 'right' }}>최종 합격자 발표하러 가기</button>
+      <div className='z-management_btn'>
+        <button className='z-buttons' style={{ float: 'right' }}>지원서 평가 기준 입력하기</button>
+        <button className='z-buttons' style={{ float: 'left' }} onClick={ onClick }>합격자 발표하러 가기</button>
       </div>
+      <div style={ {height: '80px'} }></div>
     </div>
 
   );

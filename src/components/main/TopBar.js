96a77b312c2b7a5890d355e348eb40d6e04ff708
zinzo1019 <topjoy22@naver.com>
2022-08-16 02:11:11 +0900

    feat: 메인 페이지에 네비게이션 바 추가

---
+++ src/components/main/TopBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import logoPng from '../../assets/Login/LogoPng.png'
+import '../../assets/Main/TopBar.css';
+
+function TopBar(props) {
+  return (
+    <div>
+      <nav id='nav3'>
+        <a href='#' style={ { textDecoration: 'none' }}><img className='z-top-img' src={ logoPng }/></a>
+        <ul>
+          <li><a href='#'>모집 공고 보기</a></li>
+          <li><a href='#'>내 지원 목록 확인</a></li>
+          <li><a href='http://localhost:3000/party/staff'>내 동아리 보기</a></li>
+        </ul>
+     </nav>
+    </div>
+  )
+}
+
+export default TopBar;

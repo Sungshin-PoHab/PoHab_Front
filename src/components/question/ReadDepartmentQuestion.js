513d1b1b638ee9463e41f3e4d65a2a47256cd955
eunjungL <dldmswjd322@gmail.com>
2022-08-16 02:21:46 +0900

    refactoring: ReadDepartmentQuestion refactoring

--- src/components/question/ReadDepartmentQuestion.js
+++ src/components/question/ReadDepartmentQuestion.js
@@ -3,103 +3,119 @@ import { useParams } from 'react-router-dom';
 import instance from '../../utils/axiosConfig';
 
 function ReadDepartmentQuestion() {
-  const [questionList, setQuestionList] = useState([]);
-  const [departments, setDepartments] = useState('');
+  const [questionList, setQuestionList] = useState(new Map());
+  const [departments, setDepartments] = useState([]);
   const [nowIndex, setNowIndex] = useState(0);
-  const [question, setQuestion] = useState('');
-
-  const department_ids = useRef([]);
   const { party_id } = useParams();
 
-  const deleteQuestion = async (event, question_id) => {
-    event.preventDefault();
-
+  const handleDelete = async (question_id, questionList, nowIndex) => {
     if (window.confirm('해당 질문을 정말 삭제하시겠습니까?')) {
-      // await instance.delete('/question/' + question_id);
+      await instance.delete('/question/' + question_id);
+      setQuestionList(
+        new Map([
+          ...questionList,
+          [
+            nowIndex,
+            questionList
+              .get(nowIndex)
+              .filter((question) => question.id !== question_id),
+          ],
+        ])
+      );
       alert('삭제했습니다.');
+    } else {
+      alert('취소 되었습니다.');
     }
   };
 
-  const handleDepartmentClick = (event, department_id) => {
+  const handleDepartClick = (event, department_id) => {
     event.preventDefault();
 
     setNowIndex(department_id);
   };
 
-  const getDepartments = async () => {
-    const res = await instance.get('/recruit/department/' + party_id);
-    department_ids.current.length = 0;
+  const fetchDepartments = async () => {
+    const res = await instance.get(`/recruit/department/${party_id}`);
 
-    setNowIndex(
-      res.data.find((data) => {
-        return data.department === '공통';
-      }).id
-    );
+    res.data.map((data) => {
+      questionList.set(data.id, []);
+      setQuestionList(questionList);
+    });
 
-    let max_id = 0;
-    setDepartments(
-      res.data.map((data) => {
-        if (data.department === '개인정보' || data.department === '설명') {
-          // pass
-        } else {
-          if (!department_ids.current.includes(data.id))
-            department_ids.current.push(data.id);
-          if (max_id < data.id) max_id = data.id;
-          return [
-            <button onClick={(event) => handleDepartmentClick(event, data.id)}>
-              {data.department}
-            </button>,
-          ];
-        }
-      })
-    );
-
-    setQuestionList([...Array(max_id + 1)].map(() => Array(0)));
+    let data = res.data;
+    data = data.filter((data) => data.department !== '개인정보');
+    data = data.filter((data) => data.department !== '설명');
+    setDepartments(data);
   };
 
-  const fetchQuestions = async (nowIndex) => {
-    console.log('rerender');
+  const renderDepartments = (departments) => {
+    return departments.map((department) => {
+      return [
+        <button
+          className={'L-department-btn'}
+          onClick={(event) => handleDepartClick(event, department.id)}
+        >
+          {department.department}
+        </button>,
+      ];
+    });
+  };
+
+  const fetchQuestions = async () => {
     const res = await instance.get('/question/' + party_id);
     if (res.status !== 200) {
       console.error('error!');
     }
 
-    setQuestion(
-      res.data.map((data) => {
-        if (data.department.id === nowIndex) {
-          return [
-            <div>
-              <p>{data.question}</p>
-              <p>{data.maxLength}</p>
-              <button onClick={(event) => deleteQuestion(event, data.id)}>
-                삭제
-              </button>
-            </div>,
-          ];
-        }
-      })
-    );
+    console.log(res.data.length);
+    console.log(questionList);
+    for (let i = 0; i < res.data.length; i++) {
+      questionList.set(res.data[i].department.id, [
+        ...questionList.get(res.data[i].department.id),
+        res.data[i],
+      ]);
+    }
+    setQuestionList(questionList);
+  };
+
+  const renderQuestions = (questionList, nowIndex) => {
+    if (nowIndex === 0) return [];
+    return questionList.get(nowIndex).map((question) => {
+      return [
+        <div className={'L-row'}>
+          <p>{question.question}</p>
+          <button
+            className={'L-delete-btn'}
+            onClick={() => handleDelete(question.id, questionList, nowIndex)}
+          >
+            삭제
+          </button>
+        </div>,
+      ];
+    });
   };
 
   useEffect(() => {
-    getDepartments();
+    fetchDepartments();
   }, []);
 
   useEffect(() => {
-    fetchQuestions(nowIndex);
-  }, [nowIndex]);
+    fetchQuestions();
+  }, []);
 
   return (
-    <div>
-      <div>
-        <h3>모집 부서 지정</h3>
-        <p>무집 부서 별로 지원서 질문을 다르게 지정할 수 있습니다.</p>
+    <div className={'L-container'}>
+      <div className={'L-description'}>
+        <h3 className={'L-description-title'}>모집 부서 지정</h3>
+        <p className={'L-description-context'}>
+          무집 부서 별로 지원서 질문을 다르게 지정할 수 있습니다.
+        </p>
       </div>
-      <div>
-        <p>카테고리</p>
-        {departments}
+      <div className={'L-col'}>
+        <p className={'L-p'}>카테고리</p>
+        <div className={'L-row'}>{renderDepartments(departments)}</div>
       </div>
-      <div>{question}</div>
+      {renderQuestions(questionList, nowIndex)}
     </div>
   );
 }

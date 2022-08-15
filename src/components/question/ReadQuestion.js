c2e6923907ecd07cf82b090e38fe72e481318152
eunjungL <dldmswjd322@gmail.com>
2022-08-16 01:21:28 +0900

    feat: Read Question style sheet apply

--- src/components/question/ReadQuestion.js
+++ src/components/question/ReadQuestion.js
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import axios from 'axios';
+import instance from '../../utils/axiosConfig';
+import '../../assets/question/ReadQuestion.css';
 
 function ReadQuestion() {
   const navigate = useNavigate();
@@ -15,12 +16,12 @@ function ReadQuestion() {
 
   const deleteQuestion = async (question_id) => {
     if (window.confirm('해당 질문을 정말 삭제하시겠습니까?')) {
-      await axios.delete('/question/' + question_id);
+      await instance.delete('/question/' + question_id);
     }
   };
 
   const getDepartments = async () => {
-    const res = await axios.get('/recruit/department/' + party_id);
+    const res = await instance.get('/recruit/department/' + party_id);
 
     // 설명 id get
     const description_data = res.data.find((data) => {
@@ -36,7 +37,7 @@ function ReadQuestion() {
 
   const getQuestion = useCallback(async () => {
     // 설명 get
-    const description_questions = await axios.get(
+    const description_questions = await instance.get(
       '/question?department=' + description_id.current
     );
     description_questions.data.forEach((data) => {
@@ -46,16 +47,20 @@ function ReadQuestion() {
     });
 
     // 개인정보 질문 get
-    const personal_questions = await axios.get(
+    const personal_questions = await instance.get(
       '/question?department=' + personal_id.current
     );
     setPersonalQuestions(
       personal_questions.data.map((data) => {
         return [
-          <div>
+          <div className={'L-row'}>
             <p>{data.question}</p>
-            <button>수정</button>
-            <button onClick={() => deleteQuestion(data.id)}>삭제</button>
+            <button
+              className={'L-delete-btn'}
+              onClick={() => deleteQuestion(data.id)}
+            >
+              삭제
+            </button>
           </div>,
         ];
       })
@@ -68,24 +73,29 @@ function ReadQuestion() {
   }, [getQuestion]);
 
   return (
-    <div>
-      <div>
-        <h3>설명 입력</h3>
-        <p>지원서 작성 시 제일 상단에 보여집니다.</p>
+    <div className={'L-container'}>
+      <div className={'L-description'}>
+        <h3 className={'L-description-title'}>설명 입력</h3>
+        <p className={'L-description-context'}>
+          지원서 작성 시 제일 상단에 보여집니다.
+        </p>
       </div>
-      <div>
-        <p>지원서 제목</p>
+      <div className={'L-col'}>
+        <p className={'L-p'}>지원서 제목</p>
         {title}
       </div>
-      <div>
-        <p>설명글 입력</p>
+      <div className={'L-col'}>
+        <p className={'L-p'}>설명글 입력</p>
         {description}
       </div>
-      <div>
-        <h3>개인정보 질문란</h3>
+      <div className={'L-description'}>
+        <h3 className={'L-description-title'}>개인정보 질문란</h3>
       </div>
-      <div>{personalQuestions}</div>
-      <button onClick={() => navigate(`/question/department/${party_id}`)}>
+      {personalQuestions}
+      <button
+        className={'L-button'}
+        onClick={() => navigate(`/question/department/${party_id}`)}
+      >
         지원서 질문 확인으로 넘어가기 >{' '}
       </button>
     </div>

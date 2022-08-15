efd5e68e698679a5109ed81f72b88a809239daea
eunjungL <dldmswjd322@gmail.com>
2022-08-15 23:54:29 +0900

    feat: Read Standard style sheet apply

--- src/components/gradingStandard/ReadStandard.js
+++ src/components/gradingStandard/ReadStandard.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import instance from '../../utils/axiosConfig';
+import '../../assets/ReadStandard.css';
 
 function ReadStandard() {
   const [departments, setDepartments] = useState([]);
@@ -17,11 +18,11 @@ function ReadStandard() {
     data = data.filter((data) => data.department !== '설명');
     setDepartments(data);
 
-    setNowIndex(
-      data.find((data) => {
-        return data.department === '공통';
-      }).id
-    );
+    // setNowIndex(
+    //   data.find((data) => {
+    //     return data.department === '공통';
+    //   }).id
+    // );
 
     data.map((data) => {
       standardList.set(data.id, []);
@@ -32,7 +33,10 @@ function ReadStandard() {
   const renderDepartments = (departments) => {
     return departments.map((department) => {
       return [
-        <button onClick={(event) => handleDepartClick(event, department.id)}>
+        <button
+          className={'L-department-btn'}
+          onClick={(event) => handleDepartClick(event, department.id)}
+        >
           {department.department}
         </button>,
       ];
@@ -60,9 +64,10 @@ function ReadStandard() {
     if (nowIndex === 0) return [];
     return standardList.get(nowIndex).map((standard) => {
       return [
-        <div>
+        <div className={'L-row'}>
           <p>{standard.gradingStandard}</p>
           <button
+            className={'L-delete-btn'}
             onClick={() => handleDelete(standard.id, standardList, nowIndex)}
           >
             삭제
@@ -97,13 +102,15 @@ function ReadStandard() {
   };
 
   return (
-    <div>
-      <div>
-        <h3>모집 부서 지정</h3>
-        <p>모집 부서 별로 채점 질문을 다르게 지정할 수 있습니다.</p>
-        <div>{renderDepartments(departments)}</div>
+    <div className={'L-container'}>
+      <div className={'L-description'}>
+        <h3 className={'L-description-title'}>모집 부서 지정</h3>
+        <p className={'L-description-context'}>
+          모집 부서 별로 채점 질문을 확인할 수 있습니다.
+        </p>
       </div>
-      <div>{renderStandard(standardList, nowIndex)}</div>
+      <div className={'L-row'}>{renderDepartments(departments)}</div>
+      {renderStandard(standardList, nowIndex)}
     </div>
   );
 }

155cb82b31bcb35c034527d73a0bea739e74ab33
sunseo18 <sunseo1818@gmail.com>
2022-08-15 16:33:18 +0900

    feat: 동아리 지원 내역 불러오기 (우선 Post로 )

--- src/components/myapplies/MyParties.js
+++ src/components/myapplies/MyParties.js
@@ -1,4 +1,64 @@
-import React from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
+import instance from '../../utils/axiosConfig';
 
-function MyParties() {}
+//동아리 지원 내역
+function MyParties() {
+  const [applies, setApplies] = useState();
+
+  const columns = useMemo(() => [
+    {
+      accessor: 'partyName',
+      Header: '소속',
+    },
+    {
+      accessor: 'department',
+      Header: '부서',
+    },
+    {
+      accessor: 'nth',
+      Header: '지원 기수',
+    },
+    {
+      accessor: 'step',
+      Header: '몇차',
+    },
+    {
+      accesor: 'result',
+      Header: '상태',
+    },
+    {
+      accessor: 'application',
+      Header: '지원서',
+    },
+  ]);
+
+  const getData = () => {
+    instance
+      .post(
+        `apply/myApply`,
+        {
+          id: 1,
+          name: '정준서',
+          email: 'kjmj13@navaer.com',
+        },
+        {
+          headers: {
+            Authorization: window.localStorage.getItem('authorization'),
+          },
+        }
+      )
+      .then((res) => {
+        console.log('사용자의 지원정보: ', res.data);
+        setApplies(res.data);
+
+        return res.data;
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div></div>;
+}
 export default MyParties;

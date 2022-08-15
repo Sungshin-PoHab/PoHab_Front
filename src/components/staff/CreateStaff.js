e2c61b669f4e0823bc4583895c37a83681e497e8
eunjungL <dldmswjd322@gmail.com>
2022-08-16 02:52:56 +0900

    feat: Create Staff style sheet apply

--- src/components/staff/CreateStaff.js
+++ src/components/staff/CreateStaff.js
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
 import instance from '../../utils/axiosConfig';
+import '../../assets/staff/CreateStaff.css';
 
 function CreateStaff() {
   const [role, setRole] = useState('');
@@ -45,7 +46,7 @@ function CreateStaff() {
       // 등록 성공
       alert(`${res.data.party.name}에 ${role}로 등록됐습니다.`);
       console.log(res.data);
-      navigate('/');
+      navigate('/staff/read/' + res.data.party.id);
     }
   };
 
@@ -60,25 +61,27 @@ function CreateStaff() {
           소속 생성 시 발급받은 코드를 통해 등록할 수 있습니다.
         </p>
       </div>
-      <div>
-        <p>역할</p>
+      <div className={'L-col'}>
+        <p className={'L-p'}>역할</p>
         <input
+          className={'L-input-text'}
           type={'text'}
           name={'roleInput'}
           onChange={handleChange}
           value={role}
         />
       </div>
-      <div>
-        <p>코드</p>
+      <div className={'L-col'}>
+        <p className={'L-p'}>코드</p>
         <input
+          className={'L-input-text'}
           type={'text'}
           name={'codeInput'}
           onChange={handleChange}
           value={code}
         />
       </div>
-      <input type={'submit'} value={'등록하기'} />
+      <input className={'L-submit'} type={'submit'} value={'등록하기'} />
     </form>
   );
 }

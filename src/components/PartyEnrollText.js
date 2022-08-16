d268e923cbcf00996de5bd2c49cabe42a242a51c
sunseo18 <sunseo1818@gmail.com>
2022-08-16 14:09:44 +0900

    style: Party enroll 후 코드 페이지 csss 구현

--- src/components/PartyEnrollText.js
+++ src/components/PartyEnrollText.js
@@ -7,7 +7,7 @@ function PartyText() {
       <div class="J_party_title">
         <h2>소속 등록</h2>
         <p id="J_party_expl">
-          정보를 입력해 새로운 소속을 등록할 수 있습니다. <br></br>생성자가 대표자로 등록됩니다.
+          정보를 입력해 새로운 소속을 등록할 수 있습니다. <br></br>생성자가 회장으로 등록됩니다.
         </p>
       </div>
     </div>

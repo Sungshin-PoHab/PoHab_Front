21184d22498e5865ba8a3e75095085aae5a6bda7
zinzo1019 <topjoy22@naver.com>
2022-08-18 08:12:58 +0900

    충돌

--- src/components/PartyListForStaff/PartyList.js
+++ src/components/PartyListForStaff/PartyList.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect, useParams } from 'react';
 
 import '../../assets/PartyListForStaff/PartyList.css';
 
 function PartyList(props) {
 
+  // 고쳐야 하는 부분 (링크 수정)
   const departmentClick = async (data, e) => {
-    window.location.href = `http://localhost:5000/apply/forStaff/${ data.deId }/${ data.stepId }`;
+    window.location.href = `http://localhost:5000/party/informs/${props.partyId}`;
   }
 
   return (
@@ -13,7 +14,7 @@ function PartyList(props) {
       <button className={ props.isOddNum } style={{ height: '55px', borderRadius: '10px' }}
         onClick={ (e) => {departmentClick( { deId: props.departmentId, stepId: props.stepId },  e)}}>
         <div>
-        <p className='z-party-p' style={{ color: 'black', lineHeight: '350%' }}>{ props.partyId }</p>
+        <p className='z-party-p' style={{ color: 'black', lineHeight: '350%' }}>{ props.partyId.split('-')[0] } { props.partyId.split('-')[1] }기</p>
         <p className='z-role-p' style={{ lineHeight: '380%' }}>{ props.role }</p>
         </div>
       </button>

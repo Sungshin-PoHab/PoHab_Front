0117d1adebd38182a0bb191ccb1cfb93e6310052
eunjungL <dldmswjd322@gmail.com>
2022-08-18 04:29:09 +0900

    feat: staff style sheet apply

--- src/components/staff/ReadStaff.js
+++ src/components/staff/ReadStaff.js
@@ -26,11 +26,11 @@ function ReadStaff() {
   const renderStaff = (staffList) => {
     return staffList.map((staff) => {
       return [
-        <div>
-          <p>
+        <div className={'L-row'}>
+          <p className={'L-p'}>
             {staff.user.name} {staff.role}
           </p>
-          <button onClick={() => handleDelete(staff.id, staffList)}>
+          <button className={'L-delete-btn'} onClick={() => handleDelete(staff.id, staffList)}>
             삭제
           </button>
         </div>,
@@ -43,8 +43,10 @@ function ReadStaff() {
   }, []);
 
   return (
-    <div>
-      <h3>{party_id}에 등록된 운영진 목록입니다.</h3>
+    <div className={'L-container'}>
+      <div className={'L-description'}>
+        <h3 className={'L-description-title'}>{party_id.split('-')[0]}에 등록된 운영진 목록입니다.</h3>
+      </div>
       {renderStaff(staffList)}
     </div>
   );
